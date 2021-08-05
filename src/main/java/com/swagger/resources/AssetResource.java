9fb0700edebdef6f3dc62baf0ac7082510af14da
harshapesala <pesalaharsha97@gmail.com>
2021-08-05 10:45:38 +0530

    added resource data

--- src/main/java/com/swagger/resources/AssetResource.java
+++ src/main/java/com/swagger/resources/AssetResource.java
@@ -1,5 +1,19 @@
 package com.swagger.resources;
 
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.PathParam;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+
+@Path("/asset")
 public class AssetResource {
+	@GET
+	@Produces(MediaType.TEXT_PLAIN)
+	@PathParam("/{ahn}/property")
+	public String getAssetDetails(@PathParam("ahn") String assectHolderName) {
+		return assectHolderName;
+		
+	}
 
 }
